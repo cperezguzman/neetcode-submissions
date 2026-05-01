1e7460688e8bb19818fb9b92712a1be5ba23e6e4
cperezguzman <115114613+cperezguzman@users.noreply.github.com>
2026-05-01 19:25:37 -0400

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> group;
+
+	for (const auto& word : strs) {
+	    vector<int> count(26, 0);
+	    
+	    for (const auto& ch : word) {
+	    	count[ch - 'a']++;
+	    }
+
+	    string key = to_string(count[0]);
+
+	    for (size_t i = 0; i < count.size(); i++) {
+	    	key += ("," + to_string(count[i]));
+	    }
+
+	    group[key].push_back(word);
+	}
+
+	vector<vector<string>> anagrams;
+
+	for (const auto& pair : group) {
+	    anagrams.push_back(pair.second);
+	}
+
+	return anagrams;
+	
+	}
+};
